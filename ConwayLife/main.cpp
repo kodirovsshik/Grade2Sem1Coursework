5e380c40661ac2b392c131d0aea321c272d49efe
kodirovsshik <20905994+kodirovsshik@users.noreply.github.com>
2021-11-21 03:47:13 +0300

    Sync

--- ConwayLife/main.cpp
+++ ConwayLife/main.cpp
@@ -5,26 +5,22 @@
 class game_t : public engine_t
 {
 private:
-	size_t count = 3;
 
 public:
 	virtual bool update(float dt)
 	{
-		return this->count-- != 0;
+		return !this->engine_key_down[(int)ksn::keyboard_button_t::esc];
 	}
 	virtual void draw(framebuffer_t& frame)
 	{
-
 	}
 
 	virtual void on_init()
 	{
-		this->set_window_size({ 300, 300 });
-		this->set_window_size_constraint({ 200, 400 }, {250, 500});
 		this->set_window_title(L"あ");
+		this->set_framerate_limit(60);
 
 		this->engine_use_async_displaying = false;
-		this->engine_autoclear_color = ksn::color_bgr_t(0, 0, 255);
 	}
 	virtual void on_exit()
 	{
@@ -34,7 +30,6 @@ public:
 };
 
 
-//4 byte screen alignment
 int main()
 {
 	return game_t().run();
